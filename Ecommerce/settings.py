b50ffd508851a2c6e925142fde5ca5f5558968d1
LOOP  ∞ <175203647+sarveshsinghthakur@users.noreply.github.com>
2024-10-04 18:31:03 +0530

    Update settings.py

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-5(6luu@6npnlmmip&ye)r$f^q%6lfhp-)abk89)ls^ffv6wewv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
