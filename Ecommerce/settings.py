d556126bed098e6c7a544ee25ccd1a7391e8cfa4
LOOP  ∞ <175203647+sarveshsinghthakur@users.noreply.github.com>
2024-10-04 18:50:48 +0530

    Update settings.py

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5(6luu@6npnlmmip&ye)r$f^q%6lfhp-)abk89)ls^ffv6wewv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DJANGO_DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = ["*"]
 
